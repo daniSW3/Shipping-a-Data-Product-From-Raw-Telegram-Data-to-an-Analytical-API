b22a50eaf65ad4c2b1e22fcbe38115a06b6be6db
daniSW3 <dasufr194@gmail.com>
2025-07-13 23:05:10 +0300

    Data Modeling and Transformation (Transform)

---
+++ telegrahm/telegrahm_dbt/models/marts/dim_dates.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table', schema='marts') }}
+
+WITH date_range AS (
+    SELECT generate_series('2025-07-01'::DATE, '2025-07-31'::DATE, INTERVAL '1 day') AS date
+)
+SELECT
+    date,
+    EXTRACT(YEAR FROM date) AS year,
+    EXTRACT(MONTH FROM date) AS month,
+    EXTRACT(DAY FROM date) AS day,
+    EXTRACT(DOW FROM date) AS day_of_week,
+    TO_CHAR(date, 'Day') AS day_name
+FROM date_range
