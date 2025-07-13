ad3bc7a1140700f37a43c487701502a497655f72
daniSW3 <dasufr194@gmail.com>
2025-07-13 23:27:31 +0300

    adding dbt configuration

---
+++ DBT/telegrahm_dbt/models/marts/dim_channels.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table', schema='marts') }}
+
+SELECT DISTINCT
+    channel_name,
+    CASE
+        WHEN channel_name = 'Chemed123' THEN 'https://t.me/Chemed123'
+        WHEN channel_name = 'lobelia4cosmetics' THEN 'https://t.me/lobelia4cosmetics'
+        WHEN channel_name = 'tikvahpharma' THEN 'https://t.me/tikvahpharma'
+    END AS channel_url
+FROM staging.stg_telegram_messages
---
+++ dbt-demo/telegrahm_dbt/models/staging/stg_telegram_messages.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view', schema='staging') }}
+
+SELECT
+    id,
+    channel_name,
+    date,
+    (message_data->>'id')::INTEGER AS message_id,
+    (message_data->>'date')::TIMESTAMP AS message_timestamp,
+    message_data->>'text' AS message_text,
+    (message_data->>'sender_id')::INTEGER AS sender_id,
+    (message_data->>'has_media')::BOOLEAN AS has_media,
+    message_data->>'media_type' AS media_type,
+    message_data->>'media_path' AS media_path,
+    LENGTH(message_data->>'text') AS message_length
+FROM raw.telegram_messages
